1abd5e4d7b5e38dce267dbd46352d76aed0ead2b
Anders Brams <anders@brams.dk>
2021-03-02 02:11:35 +0100

    New Homebrew release from todd

--- Formula/todd.rb
+++ Formula/todd.rb
@@ -2,7 +2,7 @@
 class Todd < Formula
   url "https://github.com/cilly-cli/binary-todd/releases/download/v1.0.0/todd.tar.gz"
   desc "Binary todd"
-  version "v1.0.0"
+  version "v1.0.1"
   homepage "https://github.com/cilly-cli/binary-todd#readme"
   sha256 "70b4992895187f9eced7cc3a06e4b66503c420d9210fa3ab7e7ffd8cdc5d4cad"
 
